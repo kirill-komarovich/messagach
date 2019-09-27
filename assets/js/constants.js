e0bc6366ee686b2759f6e4be6216a9f4e3fd435a
Kirill Komarovich <still2405@gmail.com>
2019-09-27 13:41:52 +0300

    Add authentication API

---
+++ assets/js/constants.js
@@ -0,0 +1,14 @@
+export const HTTP_METHODS = Object.freeze({
+  get: 'GET',
+  post: 'POST',
+  delete: 'DELETE',
+  put: 'PUT',
+});
+
+export const HTTP_HEADERS = Object.freeze({
+  contentType: {
+    json: {
+      'Content-Type': 'application/json',
+    },
+  },
+});
